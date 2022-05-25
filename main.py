a43c810e78678f3971330920a5566a5c6c92d55f
Diogo Medeiros <58083031+Necas209@users.noreply.github.com>
2022-05-25 23:56:16 +0100

    Start page

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from kivy.app import App
+from kivy.core.text import LabelBase
+from kivy.core.window import Window
 from kivy.uix.widget import Widget
+from kivymd.app import MDApp
 
 
 class StartPage(Widget):
@@ -7,10 +9,16 @@ class StartPage(Widget):
         super().__init__()
 
 
-class GameApp(App):
+class GameApp(MDApp):
     def build(self):
         return StartPage()
 
 
 if __name__ == '__main__':
+    LabelBase.register(name='Comic Neue',
+                       fn_regular='fonts/ComicNeue-Bold.ttf',
+                       fn_bold='fonts/ComicNeue-Bold.ttf',
+                       fn_italic='fonts/ComicNeue-Italic.ttf',
+                       fn_bolditalic='fonts/ComicNeue-BoldItalic.ttf')
+    Window.size = (600, 800)
     GameApp().run()
